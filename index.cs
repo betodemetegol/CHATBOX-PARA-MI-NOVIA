b2801d57b9e360246a5b7ee0df43f8d0565fe970
EstebanQuito <89174549+betodemetegol@users.noreply.github.com>
2021-08-18 23:50:58 -0300

    funcion paste de lo que me paso mi novia xD

---
+++ index.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace click_me
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (1 == 1)
+            {
+                Console.WriteLine("Cuando gustes te doy unas clases mamu, ingeniero en sistemas desde 2009");
+            }
+            }
+    }
+}
